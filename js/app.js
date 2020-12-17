dff4f7e10898be5bef218ac7e9889ede97785615
Bigjames728 <bigjames728@gmail.com>
2020-12-17 15:35:51 -0800

    Added some code to test my createPhrase method

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
  * Project 4 - OOP Game App
  * app.js */
 
- const phrase = new Phrase('Life is like a box of chocolate');
-
- console.log(`Phrase - phrase: ${phrase.phrase}`);
+const game = new Game();
+game.phrases.forEach((phrase, index) => {
+console.log(`Phrase ${index} - phrase: ${phrase.phrase}`);
+});
