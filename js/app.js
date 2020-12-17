6bf20b5d373e234c18c3c4adabe507024066a1f9
Bigjames728 <bigjames728@gmail.com>
2020-12-17 15:20:43 -0800

    Added in some code to App.js to test what I've built so far

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+
+ const phrase = new Phrase('Life is like a box of chocolate');
+
+ console.log(`Phrase - phrase: ${phrase.phrase}`);
