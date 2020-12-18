fd1a80293a3c5652356b92ab6e3ceb70e3303778
Bigjames728 <bigjames728@gmail.com>
2020-12-17 16:34:21 -0800

    Added some code to test progress.

--- js/app.js
+++ js/app.js
@@ -4,3 +4,6 @@
 
 // const game = new Game();
 // game.getRandomPhrase().addPhraseToDisplay();
+// const game = new Game();
+// game.startGame();
+// console.log(`Active Phrase - phrase: ${game.activePhrase.phrase}`);
