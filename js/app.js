2c7dbdc6a2ed5096f29cc41894966895f70cb416
Bigjames728 <bigjames728@gmail.com>
2020-12-22 12:32:09 -0800

    I added the eventListener to the buttons with the class .key

--- js/app.js
+++ js/app.js
@@ -14,3 +14,12 @@ document.getElementById('btn__reset').addEventListener('click', (e) => {
     game = new Game();
     game.startGame();
 });
+
+// Below, I set letterButtons = to all of the buttons with the .key class. I then iterate through each button and add an event listener to each.
+
+let letterButtons = document.querySelectorAll('.key');
+for (let i = 0; i < letterButtons.length; i++) {
+    letterButtons[i].addEventListener('click', (e) => {
+        game.handleInteraction(e.target);
+    });
+}
